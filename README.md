df59562183952cc22f89faf9fcfe34226adbd21f
suifengczc <ssuifengczc@163.com>
2020-01-01 02:53:30 +0800

    ** Xposed Hook 0.1 ** 1. ADD README.md添加功能类介绍 2. MOD 升级gradle 3. FIXED 修复HookData hook无参函数传入空的methodParams导致的bug

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # 项目介绍
-本项目时为方便逆向分析，基于Xposed框架实现的hook模块
+本项目是为方便逆向分析，基于Xposed框架实现的hook模块
 
 - 为了方便代码的实时生效实现了免重启，[参考项目](https://github.com/shuihuadx/XposedHook)
 - 为了避免在一个类中重复写太多hook代码，又频繁只是修改，导致Hook入口类冗余杂乱，所以使用了HookModule类来定义每一个需要Hook的类和具体Hook操作
@@ -9,6 +9,31 @@
     - PluginClassModule继承自HookModule， hook 插件中的类
     
 - 为方便管理需要Hook的应用，在assets下添加了hook_package.json文件，其中用json格式添加需要hook的app，可以在不重启设备的情况下动态修改需要hook的app
+
+# 主要功能类
+
+## XposedMain
+Hook功能的外部入口，由Xposed调用。通过动态加载的方式实现了免重启，基本不对这个类的代码做修改，因为当这个类的代码变化无法通过动态加载的方式实现即时生效。
+如果确实需要对这个类做修改的，修改完成后需要重启设备以使新代码生效。
+
+## HookStack
+Hook逻辑的唯一入口，在HookStack的`setHookClasses()`方法中设置了需要Hook的类，基本上在使用过程中只需要修改`setHookClasses()`中的代码动态修改需要Hook的类
+
+## HookModule
+Hook逻辑的抽象类，里面包含了`className`(被Hook的类全限定类名)，`hookDatas`(Hook数据类)，`classLoader`(构造函数传入的加载Hook类的ClassLoader)。
+一个HookModule可以包含多个HookData，表示对当前类的多个方法或参数Hook。
+
+## HookData
+一个HookData代表一个具体Hook逻辑的数据类，包含了例如被hook的方法，方法的传参类型，XC_MethodHook等。
+
+## ClassLoaderModule
+继承自HookModule，当Hook ClassLoader相关的类时使用。
+
+## PluginClassModule
+继承自HookModule，当Hook 动态加载的插件中的类时使用。
+
+## Reflector
+反射工具类，借用自滴滴的VirtualApk项目
 
 # 使用介绍
 1. 在assets下的hook_package.json下添加需要hook的包名，格式如下：
@@ -32,5 +57,7 @@
 ```
 
 4. 在修改了xposed模块代码后需要重启被hook的apk才能使新代码生效，但不需要重启设备
+
+
 
 未完待续，keep coding...
--- app/src/main/java/com/suifeng/xposedwork/hookmodule/HookData.java
+++ app/src/main/java/com/suifeng/xposedwork/hookmodule/HookData.java
@@ -9,17 +9,17 @@ public class HookData extends HookBasicData {
     /**
      * 默认当作方法名，当hook参数时，当作参数名来用
      */
-    public String methodName;
+    String methodName;
 
     /**
      * 方法传参的类型Class数组
      */
-    public Class[] methodParams;
+    private Class[] methodParams;
     /**
      * XC_MethodHook
      */
-    public XC_MethodHook methodHook;
-    public Object[] hookVariableParams;//组合了方法传参类型和XC_MethodHook
+    XC_MethodHook methodHook;
+    Object[] hookVariableParams;//组合了方法传参类型和XC_MethodHook
 
     public HookData(String methodName, Class[] methodParams, XC_MethodHook methodHook) {
         this(methodName, methodParams, methodHook, HookBasicData.HOOK_NORMAL_METHOD);
@@ -29,13 +29,16 @@ public class HookData extends HookBasicData {
         this.methodName = methodName;
         this.methodHook = methodHook;
         this.hookType = hookType;
-        if (methodParams != null ) {
+        if (methodParams != null) {
             //组合方法参数和XC_MethodHook
             this.methodParams = methodParams;
             int length = this.methodParams.length + 1;
             hookVariableParams = new Object[length];
             System.arraycopy(this.methodParams, 0, hookVariableParams, 0, length - 1);
             hookVariableParams[length - 1] = this.methodHook;
+        } else {
+            hookVariableParams = new Object[1];
+            hookVariableParams[0] = this.methodHook;
         }
 
     }
